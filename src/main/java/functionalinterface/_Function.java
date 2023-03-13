6d6139a79eb835fac765a2033cc07a95ff726b3e
Bayu K <50491719+bayukembara@users.noreply.github.com>
2023-03-13 07:46:28 +0700

    Belajar

--- src/main/java/functionalinterface/_Function.java
+++ src/main/java/functionalinterface/_Function.java
@@ -6,7 +6,7 @@ import java.util.function.Function;
 public class _Function {
     static Function<Integer, Integer> incrementByOneFunction = number -> number + 1;
     static Function<Integer, Integer> multipleBy10 = number -> number * 10;
-    BiFunction<Integer, Integer, Integer> incrementByOneAndMultiplyBiFunction = (increment, numToMultiplyBy)
+    static BiFunction<Integer, Integer, Integer> incrementByOneAndMultiplyBiFunction = (increment, numToMultiplyBy)
             -> (increment + 1) * numToMultiplyBy;
 
     public static void main(String[] args) {
@@ -26,6 +26,7 @@ public class _Function {
 
 //        BiFunction takes 2 argument and produces 1 result
 
+        System.out.println(incrementByOneAndMultiplyBiFunction.apply(4,100));
         System.out.println(incrementByOneAndMultiply(4,100));
 
     }
