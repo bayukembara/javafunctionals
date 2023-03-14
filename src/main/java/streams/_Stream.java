17ce6601b4ffb0970c17376e0381841a8d07d4b4
Bayu K <50491719+bayukembara@users.noreply.github.com>
2023-03-14 08:02:37 +0700

    Belajar

--- src/main/java/streams/_Stream.java
+++ src/main/java/streams/_Stream.java
@@ -1,23 +1,45 @@
 package streams;
 
-import imperative.Main;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static streams._Stream.Gender.*;
+
 
 public class _Stream {
 
     public static void main(String[] args) {
+        List<Person> people = List.of(new Person("Bayu", MALE),
+                new Person("Maria", FEMALE),
+                new Person("Lisa", FEMALE),
+                new Person("Jiso", FEMALE),
+                new Person("Jenni", FEMALE),
+                new Person("Kembara", PREFER_NOT_TO_SAY));
 
+        people.stream().map(person->person.gender).collect(Collectors.toSet()).forEach(System.out::println);
+//        ! or
+        people.stream().map(person->person.name).mapToInt(String::length).forEach(System.out::println);
     }
 
-    enum Gender {
-        MALE, FEMALE
-    }
+
+
+
 
     static class Person {
         private final String name;
-        private final Main.Gender gender;
+        private final String gender;
 
-        Person(String name, Main.Gender gender) {
+        Person(String name, Gender gender) {
             this.name = name;
-            this.gender = gender;
+            this.gender = String.valueOf(gender);
         }
+        @Override
+        public String toString() {
+            return "Person{" + "name='" + name + '\'' + ", gender=" + gender + '}';
+        }
+    }
+
+    enum Gender {
+        MALE, FEMALE,PREFER_NOT_TO_SAY
+    }
 }
