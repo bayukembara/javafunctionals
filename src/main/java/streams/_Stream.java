961a4f044f38b41a18052b3388c36d2aa432d026
bayu teavaro <bayu@teavaro.com>
2023-03-14 09:19:34 +0700

    Belajar

--- src/main/java/streams/_Stream.java
+++ src/main/java/streams/_Stream.java
@@ -1,11 +1,22 @@
 package streams;
 
-import imperative.Main;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static streams._Stream.Gender.MALE;
+import static streams._Stream.Gender.FEMALE;
 
 public class _Stream {
 
     public static void main(String[] args) {
+            List<Person> people = List.of(new Person("Bayu", MALE),
+                new Person("Maria", Gender.FEMALE),
+                new Person("Lisa", FEMALE),
+                new Person("Jiso", FEMALE),
+                new Person("Jenni", FEMALE),
+                new Person("Kembara", MALE));
 
+            people.stream().map(person -> person.name).forEach(System.out::println);
     }
 
     enum Gender {
@@ -14,10 +25,16 @@ public class _Stream {
 
     static class Person {
         private final String name;
-        private final Main.Gender gender;
+        private final Gender gender;
 
-        Person(String name, Main.Gender gender) {
+        Person(String name, Gender gender) {
             this.name = name;
             this.gender = gender;
         }
+
+        @Override
+        public String toString() {
+            return "Person{" + "name='" + name + '\'' + ", gender=" + gender + '}';
+        }
+    }
 }
